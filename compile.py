1756989b9b0e1743aa1261ae70a5d4864e86948d
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-01-20 16:01:25 -0700

    Changed the compile.py to have a class and modified a few things inside flatAst that is work in progress

--- compile.py
+++ compile.py
@@ -1,48 +1,56 @@
 import compiler
 from compiler.ast import *
 
-fout = open('test.s', 'w+')
 
-ast = compiler.parse("print -input() +2")
-print ast
+class flatParser:
+  def __init__(self, ast):
+    self.tmp = 0
+    self.ast = ast
+    self.flat = []
 
-def flatAst(ast):
-  if isinstance(ast,Module):
-    print "Module"
-    flatAst(ast.node)
-  elif isinstance(ast,Stmt):
-    for stmt in ast.nodes:
-      print "****Stmt Start****"
-      flatAst(stmt)
-      print "****Stmt End****"
-  elif isinstance(ast,Printnl):
-    print "Print"
-    flatAst(ast.nodes[0])
-  elif isinstance(ast,Assign):
-    print "Assign"
-    flatAst(ast.nodes[0])
-    flatAst(ast.expr)
-  elif isinstance(ast,AssName):
-    print ast
-  elif isinstance(ast,Discard):
-    print "Discard"
-  elif isinstance(ast,Const):
-    print "Const:", ast.value
-  elif isinstance(ast,Name):
-    print "Name"
-  elif isinstance(ast,Add):
-    print "Add"
-    flatAst(ast.left)
-    flatAst(ast.right)
-  elif isinstance(ast,UnarySub):
-    print "Neg"
-    flatAst(ast.expr)
-  elif isinstance(ast,CallFunc):
-    print "CallFunc"
-    print ast.node
-    print ast.args
-  else:
-    print "Ended"
+  def flatAst(self, ast):
+    if isinstance(ast,Module):
+      print "Module"
+      # return Module(None, flatAst(ast.node))
+      self.flatAst(ast.node)
+    elif isinstance(ast,Stmt):
+      for stmt in ast.nodes:
+        print "****Stmt Start****"
+        # return Stmt(self.flatAst(stmt))
+        self.flatAst(stmt)
+        print "****Stmt End****"
+    elif isinstance(ast,Printnl):
+      print "Print"
+      self.flatAst(ast.nodes[0])
+    elif isinstance(ast,Assign):
+      print "Assign"
+      self.flatAst(ast.nodes[0])
+      self.flatAst(ast.expr)
+    elif isinstance(ast,AssName):
+      print ast
+    elif isinstance(ast,Discard):
+      print "Discard"
+    elif isinstance(ast,Const):
+      print "Const:", ast.value
+      return ast
+    elif isinstance(ast,Name):
+      print "Name"
+      return ast
+    elif isinstance(ast,Add):
+      print "Add"
+      l = self.flatAst(ast.left)
+      r = self.flatAst(ast.right)
+      self.tmp += 1
+
+    elif isinstance(ast,UnarySub):
+      print "Neg"
+      self.flatAst(ast.expr)
+    elif isinstance(ast,CallFunc):
+      print "CallFunc"
+      print ast.node
+      print ast.args
+    else:
+      print "Ended"
 
 def setStack(size):
   fout.write(".globl main\n")
@@ -51,6 +59,12 @@ def setStack(size):
   fout.write("movl %esp, %ebp\n")
   fout.write("subl $%d, %%ebp\n" % size)
 
+if __name__ == "__main__":
+  fout = open('test.s', 'w+')
 
-flatAst(ast)
-#setStack(4)
+  ast = compiler.parse("x=-5\nprint x+2+input()")
+  parser = flatParser(ast)
+  print ast
+
+  parser.flatAst(parser.ast)
+  #setStack(4)
