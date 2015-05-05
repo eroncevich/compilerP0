4535027da3f242a3fc2393990050786d0f3b85bc
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-05-04 18:31:59 -0600

    changed so it folds before propagation and after as well for testing

--- compile.py
+++ compile.py
@@ -465,9 +465,10 @@ if __name__ == "__main__":
   #print ast
 
   myUnique.unique(ast)
+  ast = folder.fold(ast)
   ast = folder.propigation(ast)
+  ast = folder.fold(ast)
   print ast
-  exit()
   #print "@@@@@@@"
   #print ast,"\n"
   myHeap = Heapify(ast)
--- tests/constProp.s
+++ tests/constProp.s
@@ -4,21 +4,13 @@ main:
 	movl %esp, %ebp
 	movl $5, %eax
 	movl $1, %eax
-	movl $8, %ebx
-	movl $12, %eax
-	movl %ebx, %ebx
-	sarl $2, %ebx
-	movl %eax, %eax
-	sarl $2, %eax
-	movl %ebx, %ebx
-	addl %eax, %ebx
+	movl $4, %eax
+	movl $4, %eax
+	movl $4, %eax
+	movl $4, %ebx
 	pushl %ebx
-	call inject_int
-	addl $4, %esp
-	movl %eax,%esi
-	pushl %esi
 	call print_any
-	popl %esi
+	popl %ebx
 	movl $0,%eax
 	leave
 	ret
